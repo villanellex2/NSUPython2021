368d7e0f054e5d006197d816a595e191592e6d47
TuS <tussss89@gmail.com>
2021-02-06 18:39:53 +0700

    problems-1/problem4

---
+++ problems-1/s-naidenov/problem4.py
@@ -0,0 +1,17 @@
+lower = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight",
+         9: "nine", 10: "ten"}
+gb = "green bottle"
+hotw = "hanging on the wall,\n"
+
+def print_song():
+    i = 10
+    while i > 0:
+        print(lower[i].title() + " " + gb + "s" + " " + hotw +
+              lower[i].title() + " " + gb + "s" + " " + hotw +
+              "And if " + lower[1] + " " + gb + " " + "should accidentally fall,\n"
+                                     "There’ll be " + lower[i - 1] + " " + gb + "s" + " " + hotw)
+        print()
+        i -= 1
+
+
+print_song()
