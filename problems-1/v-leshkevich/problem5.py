5cb6a5e78d1c2c1ece11abbd221703fa32b5ae06
Jlemix <unuashechka@gmail.com>
2021-02-08 16:43:35 +0700

    problem-1-5

---
+++ problems-1/v-leshkevich/problem5.py
@@ -0,0 +1,16 @@
+def factorization(number):
+    if number == 1:
+        return [number, 1]
+    result = []
+    p = 2
+    while p * p <= number:
+        k = 0
+        while number % p == 0:
+            k += 1
+            number //= p
+        if k > 0:
+            result.append([p, k])
+        p += 1
+    if number > 1:
+        result.append([number, 1])
+    return result
