f1e5fa47d0d94e0ea5fe6b3793a3b5d9e9e963f7
TuS <tussss89@gmail.com>
2021-02-09 17:38:54 +0700

    problems-2/problem4

--- problems_2/s-naidenov/problem4.py
+++ problems_2/s-naidenov/problem4.py
@@ -1,22 +1,18 @@
 def count_substrings_in_text(p):
     file_path = input("File path ")
     count = 0
-
+    i = 0
     with open(file_path, 'r', encoding='utf-8') as source:
         seq = input("sequence to find ")
-        pos_in_text = 0
-        for line in source:
-            str_len = len(line) - 1
-            i = -1
-            while True:
-                i = line.find(seq, i + 1)
-                if i == -1:
-                    break
-                count += 1
-                p.append(i + pos_in_text)
-            pos_in_text += str_len
+        text = ''.join(source.read()[2:].split('\n'))
+        while True:
+            i = text.find(seq, i + 1)
+            if i == -1:
+                break
+            count += 1
+            if count < 6:
+                p.append(i)
     return count
-
 
 if __name__ == '__main__':
     positions = []
