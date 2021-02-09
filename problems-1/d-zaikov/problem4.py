acc2e9d3c253891fa69ada6bab82442de6267efb
dzaikov <diman2000gzz@gmail.com>
2021-02-09 13:14:48 +0700

    problem_4 fix

--- problems-1/d-zaikov/problem4.py
+++ problems-1/d-zaikov/problem4.py
@@ -1,13 +1,15 @@
 nums = ['ten', 'nine', 'eight', 'seven', 'six', 
         'five', 'four', 'three', 'two', 'one', 'no']
-line1 = ' green bottles hanging on the wall,'
+line1 = ' green bottle'
+line1_1 = ' hanging on the wall,'
 line2 = 'And if one green bottle should accidentally fall,'
-line3_1 = "There'll be "
-line3_2 = ' green bottles hanging on the wall.'
+line3 = "There'll be "
+line3_1 = ' green bottle'
+line3_2 = ' hanging on the wall.'
 for i in range(0,10):
-    print(nums[i].title() + line1)
-    print(nums[i].title() + line1)
+    print(nums[i].title() + line1 + ('s' if i != 9 else '') + line1_1)
+    print(nums[i].title() + line1 + ('s' if i != 9 else '') + line1_1)
     print(line2)
-    print(line3_1 + nums[i + 1] + line3_2)
+    print(line3 + nums[i + 1] + line3_1 + ('s' if i + 1 != 9 else '') + line3_2)
     
     
