7de2c1845243a6c91fd75e8101c6279364f9d2aa
dzaikov <diman2000gzz@gmail.com>
2021-02-09 13:37:40 +0700

    problem_4 fix

--- problems-1/d-zaikov/problem4.py
+++ problems-1/d-zaikov/problem4.py
@@ -7,8 +7,9 @@ line3 = "There'll be "
 line3_1 = ' green bottle'
 line3_2 = ' hanging on the wall.'
 for i in range(0,10):
-    print(nums[i].title() + line1 + ('s' if i != 9 else '') + line1_1)
-    print(nums[i].title() + line1 + ('s' if i != 9 else '') + line1_1)
+    s = 's' if i != 9 else ''
+    print(nums[i].title() + line1 + s + line1_1)
+    print(nums[i].title() + line1 + s + line1_1)
     print(line2)
     print(line3 + nums[i + 1] + line3_1 + ('s' if i + 1 != 9 else '') + line3_2)
     
