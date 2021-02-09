9a8f946d7b032ea8ff7b654f64cb27d58db277e1
Yakovlev <a.yakovlev@g.nsu.ru>
2021-02-09 16:32:09 +0700

    fix

--- problems-1/a-yakovlev/problem4.py
+++ problems-1/a-yakovlev/problem4.py
@@ -1,11 +1,18 @@
-nums = ['ten', 'nine', 'eight', 'seven', 'six', 
-	'five', 'four', 'three', 'two', 'one', 'no']
+nums = ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 
-line_one = ' green bottles hanging on the wall,\n'
-line_two = 'And if one green bottle should accidentally fall,\n'
+line_one = ' green bottle{} hanging on the wall,\n'
+line_two = '{} one green bottle should accidentally fall{}\n'
 line_three_1= "There'll be "
-line_three_2 = ' green bottles hanging on the wall.'
+line_three_2 = ' green bottle{} hanging on the wall.'
+   
 
-for i in range(0, len(nums)-1):
-	same_str = nums[i].capitalize() + line_one
-	print(same_str + same_str + line_two + line_three_1 + nums[i + 1] + line_three_2)
+
+for i in range(len(nums)-1, 0, -1):
+	same_str = nums[i].capitalize() + line_one.format(
+		's' if i != 1 else '')
+	print(same_str 
+		+ same_str 
+		+ line_two.format('And if' if i != 1 else 'If that', ',' if i != 1 else '') 
+		+ line_three_1 
+		+ nums[i-1] 
+		+ line_three_2.format('s' if i != 1 else ''))
