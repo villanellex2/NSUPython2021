3bace445d58b5378c08f5e4f357d9adf4e6f22fb
dzaikov <diman2000gzz@gmail.com>
2021-02-09 14:50:40 +0700

    fix

--- problems-1/d-zaikov/problem5.py
+++ problems-1/d-zaikov/problem5.py
@@ -1,16 +1,28 @@
 def factor(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[2, 0]]
     ls = []
     d = 2
+    k = 0
     while d * d <= n:
         if n % d == 0:
-            ls.append(d)
+            k += 1
             n //= d
         else:
+            if k > 0:
+                ls.append([d, k])
+            k = 0
             d += 1
+    if k > 0:
+        ls.append([d, k])
     if n > 1:
-        ls.append(n)
-    res = []
-    for i in set(ls):
-        res.append([i, ls.count(i)])
+        if len(ls) > 0 and ls[-1][0] == n:
+            ls[-1][1] += 1
+        else:
+            ls.append([n, 1])
     
-    return res
+    return ls
+    
+print(factor(int(input())))
