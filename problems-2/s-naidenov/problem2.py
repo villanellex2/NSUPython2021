0340d8ea2b2a166feacca32502d93332e38efd18
TuS <tussss89@gmail.com>
2021-02-09 17:36:15 +0700

    problems-2/problem2

--- problems-2/s-naidenov/problem2.py
+++ problems-2/s-naidenov/problem2.py
@@ -1,16 +1,16 @@
 import re
 
-if __name__ == '__main__':
-    dictionary = {}
-    file_path = input()
-    with open(file_path, 'r', encoding='utf-8') as source:
-        for line in source:
-            s = re.findall(r"[\w']+", line)
-            for word in s[1:]:
-                if not (word in dictionary.keys()):
-                    dictionary[word] = [s[0]]
-                else:
-                    dictionary[word].append(s[0])
-        sorted_dictionary = {k: dictionary[k] for k in sorted(dictionary)}
-        for k in sorted_dictionary:
-            print(k + " - " + str(dictionary[k]))
+
+dictionary = {}
+file_path = input()
+with open(file_path, 'r', encoding='utf-8') as source:
+    for line in source:
+        s = re.findall(r"[\w']+", line)
+        for word in s[1:]:
+            if not (word in dictionary.keys()):
+                dictionary[word] = [s[0]]
+            else:
+                dictionary[word].append(s[0])
+    keys = sorted(list(dictionary.keys()))
+    for k in keys:
+        print(k + " - " + str(dictionary[k]))
