57fd636290092b8133c95c3def42234a706ffc93
Yakovlev <a.yakovlev@g.nsu.ru>
2021-02-09 14:43:38 +0700

    fixed

--- problems-1/a-yakovlev/problem2.py
+++ problems-1/a-yakovlev/problem2.py
@@ -6,3 +6,9 @@ def seq_in_range(seq, a, b):
 			seq[i] = a
 		if seq[i] > b:
 			seq[i] = b
+
+	return seq
+
+seq = list(map(lambda x: int(x), input().split()))
+
+print(seq_in_range(seq, int(input()), int(input())))
