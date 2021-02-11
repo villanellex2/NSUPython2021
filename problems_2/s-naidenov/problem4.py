cd4159f312434ad28fdbeb2fd3dd1da966380524
TuS <tussss89@gmail.com>
2021-02-11 11:14:13 +0700

    return tuple in problems-2/problem4

--- problems_2/s-naidenov/problem4.py
+++ problems_2/s-naidenov/problem4.py
@@ -1,4 +1,5 @@
-def count_substrings_in_text(p):
+def count_substrings_in_text():
+    p = []
     file_path = input("File path ")
     count = 0
     i = 0
@@ -12,10 +13,9 @@ def count_substrings_in_text(p):
             count += 1
             if count < 6:
                 p.append(i)
-    return count
+    return count, p
 
 if __name__ == '__main__':
-    positions = []
-    res = count_substrings_in_text(positions)
-    print("Found " + str(res))
-    print("Positions " + str(positions[:5]))
+    count, positions = count_substrings_in_text()
+    print("Found " + str(count))
+    print("Positions " + str(positions))
