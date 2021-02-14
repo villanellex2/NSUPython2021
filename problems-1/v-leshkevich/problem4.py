e1f95cd4af6e47f0283b07b2c1f0385772f24967
Jlemix <unuashechka@gmail.com>
2021-02-14 23:12:54 +0700

    fix

--- problems-1/v-leshkevich/problem4.py
+++ problems-1/v-leshkevich/problem4.py
@@ -1,13 +1,15 @@
 def print_green_bottles_text():
     numbers = ['ten', 'nine', 'eight', 'seven', 'six',
                'five', 'four', 'three', 'two', 'one', 'no']
-    hanging_text = " green bottles hanging on the wall"
+    hanging_text = " green bottle{s} hanging on the wall"
     i = 0
     for number in numbers[:10]:
         i = i + 1
-        print(number.title() + hanging_text, end=',\n')
-        print(number.title() + hanging_text, end=',\n')
+        print(number.title() + hanging_text.format(s="s" if i != 10 else ''), end=',\n')
+        print(number.title() + hanging_text.format(s="s" if i != 10 else ''), end=',\n')
         print("{text} one green bottle should accidentally fall,\nThere'll be "
-              .format(text="And if" if i < 10 else "If that") + numbers[i] + hanging_text, end='.\n')
+              .format(text="And if" if i < 10 else "If that") + numbers[i] +
+              hanging_text.format(s="s"), end='.\n')
+
 
 print_green_bottles_text()
