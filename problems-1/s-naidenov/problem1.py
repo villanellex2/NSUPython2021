a422b5b39671d52e6ca4d078c03ecff00a563e0c
Sergey <tussss89@gmail.com>
2021-02-15 13:03:25 +0700

    add "if _main_"

--- problems-1/s-naidenov/problem1.py
+++ problems-1/s-naidenov/problem1.py
@@ -7,8 +7,10 @@ def cum_sum(l):
         res.append(cur_sum)
     return res
 
-s = input().split(" ")
-l = []
-for i in s:
-    l.append(int(i))
-print(str(cum_sum(l)))
+
+if __name__ == '__main__':
+    s = input().split(" ")
+    l = []
+    for i in s:
+        l.append(int(i))
+    print(str(cum_sum(l)))
