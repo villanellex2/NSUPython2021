3c730b35158b70e3aa6581e2098865536890a5fa
Simon Morozov <morozzmorozov@gmail.com>
2021-02-15 15:43:01 +1100

    problem-4 fix

--- problems-1/s-morozov/problem4.py
+++ problems-1/s-morozov/problem4.py
@@ -6,4 +6,4 @@ for i in range(10, 0, -1):
   for j in range(2):
     print(nums[i].capitalize() + " green bottle" + ("" if i == 1 else "s") + " hanging on the wall,")
   print(("If that" if i == 1 else "And if") + " one green bottle should accidentally fall,")
-  print("There'll be " + nums[i - 1] +  " green bottle" + ("" if i == 1 else "s") + " hanging on the wall.")
+  print("There'll be " + nums[i - 1] +  " green bottle" + ("" if i == 2 else "s") + " hanging on the wall.")
