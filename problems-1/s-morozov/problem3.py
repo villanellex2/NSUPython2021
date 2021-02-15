fdfaad846e3a751a37cdc9c639522f8f11bb7081
Simon Morozov <morozzmorozov@gmail.com>
2021-02-15 13:29:32 +1100

    problem-3 update

--- problems-1/s-morozov/problem3.py
+++ problems-1/s-morozov/problem3.py
@@ -3,10 +3,7 @@
 def collatz(n):
   chain = [n]
   while chain[-1] != 1:
-    if chain[-1] % 2 == 0:
-      chain.append(chain[-1] // 2)
-    else:
-      chain.append(3 * chain[-1] + 1)
+    chain.append(chain[-1] // 2 if chain[-1] % 2 == 0 else 3 * chain[-1] + 1)
   return chain
 
 n = int(input())
