2b16220524cf49329c06809a43c985a37823b9bd
Sergey <tussss89@gmail.com>
2021-02-15 13:05:15 +0700

    added "if _main_"

--- problems-1/s-naidenov/main.py
+++ problems-1/s-naidenov/main.py
@@ -1,9 +1,9 @@
 from problem2 import trim_sequence
-
-s = input("seq = ").split(" ")
-a = int(input("min = "))
-b = int(input("max = "))
-seq = []
-for i in s:
-    seq.append(int(i))
-print(str(trim_sequence(seq, a, b)))
+if __name__ == '__main__':
+    s = input("seq = ").split(" ")
+    a = int(input("min = "))
+    b = int(input("max = "))
+    seq = []
+    for i in s:
+        seq.append(int(i))
+    print(str(trim_sequence(seq, a, b)))
