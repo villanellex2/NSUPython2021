c58c5b1f2dc5dbb26b4a9b250dbebb969f02fe28
TuS <tussss89@gmail.com>
2021-02-16 13:31:31 +0700

    problems_1/problem2

--- problems-1/s-naidenov/main.py
+++
@@ -1,9 +0,0 @@
-from problem2 import trim_sequence
-if __name__ == '__main__':
-    s = input("seq = ").split(" ")
-    a = int(input("min = "))
-    b = int(input("max = "))
-    seq = []
-    for i in s:
-        seq.append(int(i))
-    print(str(trim_sequence(seq, a, b)))
--- problems-1/s-naidenov/problem2.py
+++ problems-1/s-naidenov/problem2.py
@@ -7,5 +7,11 @@ def trim_sequence(seq, a, b):
     return seq
 
 
-
-
+if __name__ == "__main__":
+    s = input("seq = ").split(" ")
+    a = int(input("min = "))
+    b = int(input("max = "))
+    seq = []
+    for i in s:
+        seq.append(int(i))
+    print(str(trim_sequence(seq, a, b)))
