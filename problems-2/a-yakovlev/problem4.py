ca91ef92335efdec41e686542c894d52ee21ab14
Yakovlev <a.yakovlev@g.nsu.ru>
2021-02-17 14:17:04 +0700

    problem-2-4

---
+++ problems-2/a-yakovlev/problem4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+seq = input('Enter seq: ')
+
+cnt = 0
+first_five = []
+
+with open('pi.txt', 'r') as pi:
+    str_pi = ''.join(pi.read()[2:].split())
+
+    cur_pos = str_pi.find(seq)
+    while cur_pos != -1:
+        if cnt < 5:
+            first_five.append(str(cur_pos))
+        cnt += 1
+        cur_pos = str_pi.find(seq, cur_pos+1)
+
+print(f'Found {cnt} results.\nPositions: {" ".join(first_five)} ...')
