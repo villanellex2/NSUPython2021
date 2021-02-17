4b4a664263fddceaf5447b71caf928e32cfe3a74
Yakovlev <a.yakovlev@g.nsu.ru>
2021-02-17 13:09:19 +0700

    problem-2-3

---
+++ problems-2/a-yakovlev/problem3.py
@@ -0,0 +1,21 @@
+from os import listdir, stat
+from os.path import isfile, join, exists
+from sys import argv, exit, stderr
+
+from operator import itemgetter
+
+try:
+	if len(argv) != 2:
+		print("Should be the dir path as the only argument", file=stderr)
+	else:
+		files = []
+		for name in listdir(argv[1]):
+			full_path = join(argv[1], name)
+			if isfile(full_path): 
+				files.append((name, stat(full_path).st_size))
+
+		for name, size in sorted(files, key=itemgetter(1, 0), reverse=True):
+			print(name, size)
+
+except (NotADirectoryError, FileNotFoundError):
+	print('You must specify dir path', file=stderr)
