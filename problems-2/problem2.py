cc870e00064319efd722cd5b9dc75e975a31da5e
Jlemix <unuashechka@gmail.com>
2021-02-20 22:07:01 +0700

    problem-2-2

---
+++ problems-2/problem2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import re
+
+path = input()
+dictionary = {}
+
+try:
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            result = re.split(" - |, ", line)
+            result[-1] = result[-1].rstrip('\r\n')
+            value = result.pop(0)
+            for key in result:
+                if key in dictionary:
+                    dictionary[key].append(value)
+                else:
+                    dictionary[key] = [value]
+
+    for key, value in sorted(dictionary.items()):
+        print(key + ' -', ', '.join(value))
+
+except FileNotFoundError:
+    print("Can not find file with given path.")
