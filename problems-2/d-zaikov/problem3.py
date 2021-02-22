9f8205039503f6fc5ff820530d6021b3ada2ea5d
dzaikov <diman2000gzz@gmail.com>
2021-02-23 01:25:46 +0700

    problems2/problem-3

---
+++ problems-2/d-zaikov/problem3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print('Directory path not found, please use it as argument')
+    exit()
+
+if sys.argv[1] == 'help':
+    print('Please use directory path as first argument')
+    exit()
+try:
+    ls = sorted([y for y in [os.path.join(sys.argv[1], x) 
+                       for x in os.listdir(sys.argv[1])]
+                   if os.path.isfile(y)], 
+                       key=(lambda x: (-os.stat(x).st_size, x)))
+    print([os.path.split(x)[-1] for x in ls])
+except:
+    print('Something wrong with directory path')
