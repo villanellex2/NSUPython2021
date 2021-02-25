e78976b336d867a94a2294a70f55d3b0b506f86f
a1usha <aleksandr.ushaev@hotmail.com>
2021-02-25 11:00:39 +0700

    move while

--- problems-2/a-ushaev/problem4.py
+++ problems-2/a-ushaev/problem4.py
@@ -8,12 +8,13 @@ match_cnt = 0
 with open('pi.txt', 'r') as pi:
     digits = ''.join(pi.read()[2:].split('\n'))
     cur_pos = digits.find(seq, 0)
-    while cur_pos != -1:
-        if match_cnt < 5:
-            match.append(cur_pos)
-        match_cnt += 1
-        cur_pos += 1
-        cur_pos = digits.find(seq, cur_pos)
+
+while cur_pos != -1:
+    if match_cnt < 5:
+        match.append(cur_pos)
+    match_cnt += 1
+    cur_pos += 1
+    cur_pos = digits.find(seq, cur_pos)
 
 print(f'Found {match_cnt} results.')
 print(f'Positions: {" ".join(map(str, match))} ...')
