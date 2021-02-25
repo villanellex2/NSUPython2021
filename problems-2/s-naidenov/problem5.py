078b688ad5b8f35920e841dee4a7b33853e33aa1
TuS <tussss89@gmail.com>
2021-02-25 11:14:43 +0700

    remove function

--- problems-2/s-naidenov/problem5.py
+++ problems-2/s-naidenov/problem5.py
@@ -1,15 +1,8 @@
 from math import sqrt
 
 
-def is_prime(n):
-    if n < 2:
-        return False
-    max_factor = int(sqrt(n))
-    return all(n % i for i in range(2, max_factor + 1))
-
-
 def generate_primes(n):
-    return [i for i in range(2, n + 1) if is_prime(i)]
+    return [i for i in range(2, n + 1) if all(i % d for d in range(2, int(sqrt(i)) + 1))]
 
 
 if __name__ == "__main__":
