52516bf7b4829924178668a97815b22efac94420
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-02-26 12:27:32 +0700

    Handle exceptions in problem2-3

--- problems-2/o-markelov/problem3.py
+++ problems-2/o-markelov/problem3.py
@@ -4,12 +4,24 @@ from os import listdir, stat
 from os.path import isfile, join
 from sys import argv
 
-files_info = []
+if len(argv) < 2:
+    print('You haven\'t passed the path argument')
+    exit()
 
-for filename in listdir(argv[1]):
-    full_filename = join(argv[1], filename)
-    if isfile(full_filename):
-        files_info.append((filename, stat(full_filename).st_size))
+try:
+    files_info = []
 
-for file_info in sorted(files_info, key=lambda info: (-info[1], info[0])):
-    print(file_info[0], ' - ', file_info[1], 'bytes')
+    for filename in listdir(argv[1]):
+        full_filename = join(argv[1], filename)
+        if isfile(full_filename):
+            files_info.append((filename, stat(full_filename).st_size))
+
+    if not files_info:
+        print('No files')
+        exit()
+
+    for file_info in sorted(files_info, key=lambda info: (-info[1], info[0])):
+        print(file_info[0], ' - ', file_info[1], 'bytes')
+
+except OSError as e:
+    print(f'Cannot open "{argv[1]}" ({e.strerror})')
