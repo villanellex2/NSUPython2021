3719ccafcf2e982eb4014b5b14cc87c8306b896d
dzaikov <diman2000gzz@gmail.com>
2021-03-02 14:13:44 +0700

    fix

--- problems-2/d-zaikov/problem3.py
+++ problems-2/d-zaikov/problem3.py
@@ -2,19 +2,41 @@
 
 import sys
 import os
+from optparse import OptionParser
 
-if len(sys.argv) < 2:
-    print('Directory path not found, please use it as argument')
-    exit()
+parser = OptionParser()
+parser.add_option("-p", "--path", dest="dirpath",
+                    help="Scan directory by path.")
+                    
+(options, args) = parser.parse_args()
 
-if sys.argv[1] == 'help':
-    print('Please use directory path as first argument')
+if options.dirpath is None:
+    print("Directory not specified, use -p to write path.",
+           file=sys.stderr)
     exit()
+    
+if not os.path.exists(options.dirpath):
+    print('Directory not found', 
+           file=sys.stderr)
+    exit(1)
+    
+if not os.path.isdir(options.dirpath):
+    print("Path is not a directory.",
+           file=sys.stderr)
+    exit(1)
+
+if not os.access(options.dirpath, os.X_OK | os.R_OK):
+    print("Permission denied.",
+           file=sys.stderr)
+    exit(1)
+
+
 try:
-    ls = sorted([y for y in [os.path.join(sys.argv[1], x) 
-                       for x in os.listdir(sys.argv[1])]
+    ls = sorted([y for y in [os.path.join(options.dirpath, x) 
+                       for x in os.listdir(options.dirpath)]
                    if os.path.isfile(y)], 
                        key=(lambda x: (-os.stat(x).st_size, x)))
     print([os.path.split(x)[-1] for x in ls])
-except IOError as e:
+    
+except OSError as e:
     print(e, file=sys.stderr)
