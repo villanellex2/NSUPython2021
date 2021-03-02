0a9d3bc9ab038ee00f07c46564d7f2d0e0acc144
Yakovlev <a.yakovlev@g.nsu.ru>
2021-03-02 10:33:23 +0700

    fixed

--- problems-2/a-yakovlev/problem5.py
+++ problems-2/a-yakovlev/problem5.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 from sys import stderr
 
-def __is_prime(num):
-	return all(num % delim != 0 for delim in range(2, int(num ** 0.5) + 1))
-
 try:
 	num = int(input())
 	print([
-		i for i in range(2, num + 1) if __is_prime(i)
+		i for i in range(2, num + 1) if all(i % delim != 0 for delim in range(2, int(i ** 0.5) + 1))
 	]) 
 
 except ValueError:
