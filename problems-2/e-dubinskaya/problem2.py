e9e565c54cff1f788d924b4fd0c4746a5094a87c
katerina <bromkate@mail.ru>
2021-03-02 14:18:49 +0700

    Added problem 2-2

---
+++ problems-2/e-dubinskaya/problem2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#путь до словаря из примера: res/en_dictionary.txt
+
+def add_to_latin_dict(latin_dict: dict, read_str: str):
+    read_str = read_str.replace(" ", "")
+    read_str = read_str.replace("\n", "")
+    word_meaning = read_str.split("-", 2)
+    word = word_meaning[0]
+    meaning = word_meaning[1].split(",")
+    for i in range(len(meaning)):
+        if latin_dict.get(meaning[i]) != None:
+            latin_dict[meaning[i]].append(word)
+        else:
+            latin_dict[meaning[i]] = [word]
+
+print("Enter path to dictionary file: ", end="")
+fileName = input()
+latin = {}
+try:
+    f = open(fileName, 'r')
+    for line in f:
+        add_to_latin_dict(latin, line)
+    for key in sorted(latin):
+        print(key, end=' - ')
+        values = latin.get(key)
+        size = len(values)
+        for i in range(size):
+            if i == size-1:
+                print(values[i])
+            else:
+                print(values[i], end=', ')
+except OSError:
+    print("Invalid input")
