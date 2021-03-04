10a0072086d3d565c63d7ef6e44f07f92e927057
katerina <bromkate@mail.ru>
2021-03-04 12:08:36 +0700

    Update problem4.py

--- problems-2/e-dubinskaya/problem4.py
+++ problems-2/e-dubinskaya/problem4.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 
-def string_finder(string: str, value: str, res: list) -> int:
+def string_finder(string: str, value: str, res: list, prev_len: int) -> int:
     num = 0
     while True:
         curr_occ = string.find(value)
         if curr_occ == -1:
             break
         if len(res) < 5:
-            res.append(curr_occ)
+            res.append(curr_occ + prev_len)
         num += 1
         string = string[curr_occ+1:]
     return num
@@ -25,16 +25,25 @@ try:
     f = open("res/pi.txt", 'r')
 
     string = f.read(size * 10)
+    string = string[2:]
     out = 0
     res = []
+    prev_len = 0
     while True:
-        out += string_finder(string, inp, res)
+        out += string_finder(string, inp, res, prev_len)
         if len(string) < size - 1:
             break
+        prev_len += len(string) - size + 1
         string = string[-size+1:] + f.read(size * 10)
-        if len(string) == size-1:
+        if len(string) <= size-1:
             break
-    print(out)
+    print("Found " + str(out) + " results.")
+    print("Positions: ", end=" ")
+    for pos in res:
+        print(pos, end=" ")
+    if len(res) < out:
+        print("...")
+
 except OSError as e:
     from sys import stderr
     print(e, file=stderr)
