4e98f44cb1416303d45c3a7a08fde7595ea5e25a
pkaraseva18214 <karasyova_polina@mail.ru>
2021-03-08 11:06:41 +0700

    changed tests

---
+++ problems-1/p-karaseva/tests-for-problem-1-1.py
@@ -0,0 +1,25 @@
+import unittest
+from problem1 import cumsum
+
+class MyTestCase(unittest.TestCase):
+    def test_1(self):
+        data = ['a', 'b', 'c']
+        with self.assertRaises(TypeError):
+            cumsum(['a', 'v', 'd'])
+
+    def test_2(self):
+        data = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(cumsum(data), [0, 1, 3, 6, 10, 15, 21])
+
+    def test_3(self):
+        data = [i for i in range(1, 100)]
+        self.assertEqual(len(cumsum(data)), 100)
+
+    def test_4(self):
+        data = [1, 3, 4.15, 3.709, 5, 12.04]
+        self.assertEqual(cumsum(data)[-1], 28.899)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
