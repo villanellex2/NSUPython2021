f0b11b0b005223ae6f283ab9958a77e13c67ed36
pkaraseva18214 <karasyova_polina@mail.ru>
2021-03-08 11:12:32 +0700

    changed

--- problems-1/p-karaseva/problem4.py
+++ problems-1/p-karaseva/problem4.py
@@ -4,15 +4,15 @@ bottles = ['Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two
 line = ' green bottle{s} sitting on the wall'
 
 def add_S_Or_Not(i):
-    if (i + 1) != 9:
+    if (i) != 9:
         return 's'
     else:
         return ''
 
-
-for i in range(10):
-    print(bottles[i] + line.format(s='s' if i != 9 else '') + ',')
-    print(bottles[i] + line.format(s='s' if i != 9 else '') + ',')
-    print('And if one green bottle should accidentally fall')
-    print('There would be ' + bottles[i + 1].lower() + line.format(s=add_S_Or_Not(i)), end='.\n\n')
+if __name__ == '__main__':
+    for i in range(10):
+        print(bottles[i] + line.format(s=add_S_Or_Not(i)) + ',')
+        print(bottles[i] + line.format(s=add_S_Or_Not(i)) + ',')
+        print('And if one green bottle should accidentally fall')
+        print('There would be ' + bottles[i + 1].lower() + line.format(s=add_S_Or_Not(i+1)), end='.\n\n')
 
