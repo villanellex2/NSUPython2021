171c6cf247cc21ed64db581a6a146ab876b428f0
dzaikov <diman2000gzz@gmail.com>
2021-03-09 14:42:31 +0700

    fix

--- problems-2/d-zaikov/problem4.py
+++ problems-2/d-zaikov/problem4.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import unittest
 import sys
 import os
 from optparse import OptionParser
@@ -11,7 +10,7 @@ def find_seq(seq, path):
     #Оставил, так как open выдает PermissionError при чтении директории
     #(на windows)
     if os.path.isdir(path):
-        raise IsADirectoryError
+        raise IsADirectoryError('The entered path is not a file')
         return cnt, pos_list
     
     with open(path, 'r') as file:
@@ -47,20 +46,26 @@ if __name__ == '__main__':
         exit(1)
     try:
         cnt, posl = find_seq(seq, path)
-    except FileNotFoundError:
-        print('File not found', 
-               file=sys.stderr)
+    except FileNotFoundError as e:
+        print('Error: the entered path to the file with pi number not found.\n', file=sys.stderr)
+        print('--------More info--------', file=sys.stderr)
+        print(e, file=sys.stderr)
         exit(1)
-    except IsADirectoryError:
-        print('Path is not a file',
-               file=sys.stderr)
+    except IsADirectoryError as e:
+        print('Error: the entered path to the file with pi number is not a file.\n', file=sys.stderr)
+        print('--------More info--------', file=sys.stderr)
+        print(e, file=sys.stderr)
         exit(1)
-    except PermissionError:
-        print("Permission denied.",
-               file=sys.stderr)
+    except PermissionError as e:
+        print('Error: you have not permission to read the file with pi number by the entered path.\n', file=sys.stderr)
+        print('--------More info--------', file=sys.stderr)
+        print(e, file=sys.stderr)
         exit(1)
     except Exception as e:
+        print('Error: some error occurred while trying to read the file with pi number.\n', file=sys.stderr)
+        print('--------More info--------', file=sys.stderr)
         print(e, file=sys.stderr)
+        exit(1)
     print(f'Found {cnt} results.')
     print(f'Positions: ', end='')
     for p in posl:
