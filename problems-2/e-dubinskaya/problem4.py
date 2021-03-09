607f98f70389de47795cbdfd5a818c838747404a
katerina <bromkate@mail.ru>
2021-03-09 08:46:25 +0700

    Fixed reading file

--- problems-2/e-dubinskaya/problem4.py
+++ problems-2/e-dubinskaya/problem4.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 
-def string_finder(string: str, value: str, res: list, prev_len: int) -> int:
+def string_finder(string: str, value: str, res: list) -> int:
     num = 0
+    prev_len = 0
     while True:
         curr_occ = string.find(value)
         if curr_occ == -1:
@@ -11,6 +12,7 @@ def string_finder(string: str, value: str, res: list, prev_len: int) -> int:
             res.append(curr_occ + prev_len)
         num += 1
         string = string[curr_occ+1:]
+        prev_len += curr_occ + 1
     return num
 
 
@@ -24,19 +26,11 @@ size = len(inp)
 try:
     f = open("res/pi.txt", 'r')
 
-    string = f.read(size * 10)
+    string = f.read()
     string = string[2:]
-    out = 0
     res = []
     prev_len = 0
-    while True:
-        out += string_finder(string, inp, res, prev_len)
-        if len(string) < size - 1:
-            break
-        prev_len += len(string) - size + 1
-        string = string[-size+1:] + f.read(size * 10)
-        if len(string) <= size-1:
-            break
+    out = string_finder(string, inp, res)
     print("Found " + str(out) + " results.")
     print("Positions: ", end=" ")
     for pos in res:
