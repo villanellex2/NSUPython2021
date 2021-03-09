9c600b0d338adde00e6c0f1b7cfe988697070205
dzaikov <diman2000gzz@gmail.com>
2021-03-09 13:44:46 +0700

    fix

--- problems-2/d-zaikov/problem5-test.py
+++ problems-2/d-zaikov/problem5-test.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 import unittest
-from problem5 import primes_list
+from problem5 import primes_list_list_expression
+from problem5 import primes_list_sieve
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_neg12(self):
-        self.assertEqual(primes_list(-12), [])
+    def test_neg12_list_expression(self):
+        self.assertEqual(primes_list_list_expression(-12), [])
 
-    def test_pos12(self):
-        self.assertEqual(primes_list(12), [2,3,5,7,11])
+    def test_pos12_list_expression(self):
+        self.assertEqual(primes_list_list_expression(12), [2,3,5,7,11])
     
-    def test_pos11(self):
-        self.assertEqual(primes_list(12), [2,3,5,7,11])
+    def test_pos11_list_expression(self):
+        self.assertEqual(primes_list_list_expression(12), [2,3,5,7,11])
 
-    def test_exception_type_error(self):
-        self.assertRaises(TypeError, primes_list, '1111')
+    def test_exception_type_error_list_expression(self):
+        self.assertRaises(TypeError, primes_list_list_expression, '1111')
+        
+    def test_neg12_sieve(self):
+        self.assertEqual(primes_list_sieve(-12), [])
+
+    def test_pos12_sieve(self):
+        self.assertEqual(primes_list_sieve(12), [2,3,5,7,11])
+    
+    def test_pos11_sieve(self):
+        self.assertEqual(primes_list_sieve(12), [2,3,5,7,11])
+
+    def test_exception_type_error_sieve(self):
+        self.assertRaises(TypeError, primes_list_sieve, '1111')
 
 
 if __name__ == '__main__':
--- problems-2/d-zaikov/problem5.py
+++ problems-2/d-zaikov/problem5.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 import math
 import sys
-def primes_list(n):
+def primes_list_list_expression(n):
     return [i for i in range(2, n + 1) 
             if all(i % d != 0 
             for d in range(2, int(math.sqrt(i)) + 1))]
             
+def primes_list_sieve(n):
+    if n < 2:
+        return []
+    s = [True] * n
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if s[i]:
+            s[i * i::2 * i] = [False] * ((n - i * i - 1) // (2 * i) + 1)
+    return [2] + [i for i in range(3, n, 2) if s[i]]
+            
             
 if __name__ == '__main__':
+    print('Write upper bound for primes list (an integer).')
     try:
         n = int(input())
-    except ValueError:
-        print('Please, write an integer.', file=sys.stderr)
+    except ValueError as e:
+        print('Error: the entered string is not an integer.\n', file=sys.stderr)
+        print('--------More info--------')
+        print(e, file=sys.stderr)
         exit(1)
     except Exception as e:
         print(e, file=sys.stderr)
         exit(1)
      
-    print(primes_list(n))
+    print(primes_list_sieve(n))
