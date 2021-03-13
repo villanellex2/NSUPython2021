c4cfdebc28119cb8a1c7c65bae15e4f0d55c73c0
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-03-13 11:39:56 +0700

    Split text with regex

--- problems-6/o-markelov/problem1.py
+++ problems-6/o-markelov/problem1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 import sys
 from random import sample
 
@@ -23,26 +24,12 @@ else:
     sys.stderr.write('You must provide either some text or some file')
     exit()
 
-text_parts = []
-part_len = 0
-previous_alpha = text[0].isalpha()
-i = 1
-while i < len(text):
-    if previous_alpha == text[i].isalpha():
-        part_len += 1
-    else:
-        text_parts.append(text[i - 1 - part_len: i])
-        part_len = 0
-        previous_alpha = not previous_alpha
-    i += 1
-text_parts.append(text[i - 1 - part_len: i])
-
 
 def rearrange(letters):
     return sorted(letters) if args.sort else sample(letters, len(letters))
 
 
-for part in text_parts:
+for part in re.split('([^\\W\\d_]+)', text):
     if len(part) > 3 and part[0].isalpha():
         print(part[0], ''.join(rearrange(part[1:-1])), part[-1], sep='', end='')
     else:
