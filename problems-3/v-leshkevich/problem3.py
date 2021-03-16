8c375a5e8ecdef0dd0f242757822a36792e08b97
Jlemix <unuashechka@gmail.com>
2021-03-16 14:54:00 +0700

    make 'values' field private

--- problems-3/v-leshkevich/problem3.py
+++ problems-3/v-leshkevich/problem3.py
@@ -1,17 +1,17 @@
 class Vector:
     def __init__(self, *args):
         if len(args) == 0:
-            self.values = (0, 0)
+            self.__values = (0, 0)
         elif isinstance(args[0], (list, tuple)):
-            self.values = args[0]
+            self.__values = args[0]
         else:
-            self.values = args
+            self.__values = args
 
     def __add__(self, other):
         if isinstance(other, Vector):
-            result = tuple(x + y for x, y in zip(self.values, other.values))
+            result = tuple(x + y for x, y in zip(self.__values, other.__values))
         elif isinstance(other, (int, float)):
-            result = tuple(x + other for x in self.values)
+            result = tuple(x + other for x in self.__values)
         else:
             raise ValueError("Can not add with type {}".format(type(other)))
         return self.__class__(*result)
@@ -24,9 +24,9 @@ class Vector:
 
     def __sub__(self, other):
         if isinstance(other, Vector):
-            result = tuple(x - y for x, y in zip(self.values, other.values))
+            result = tuple(x - y for x, y in zip(self.__values, other.__values))
         elif isinstance(other, (int, float)):
-            result = tuple(x - other for x in self.values)
+            result = tuple(x - other for x in self.__values)
         else:
             raise ValueError("Can not subtract with type {}".format(type(other)))
         return self.__class__(*result)
@@ -39,9 +39,9 @@ class Vector:
 
     def __mul__(self, other):
         if isinstance(other, Vector):
-            return sum(x * y for x, y in zip(self.values, other.values))
+            return sum(x * y for x, y in zip(self.__values, other.__values))
         elif isinstance(other, (int, float)):
-            result = tuple(x * other for x in self.values)
+            result = tuple(x * other for x in self.__values)
             return self.__class__(*result)
         else:
             raise ValueError("Can not multiply with type {}".format(type(other)))
@@ -53,16 +53,16 @@ class Vector:
         return self.__mul__(other)
 
     def __eq__(self, other):
-        return self.values == other.values
+        return self.__values == other.__values
 
     def __getitem__(self, key):
-        return self.values[key]
+        return self.__values[key]
 
     def __repr__(self):
-        return 'Vector({args})'.format(args=self.values)
+        return 'Vector({args})'.format(args=self.__values)
 
     def __str__(self):
-        return str(self.values)
+        return str(self.__values)
 
     def __len__(self):
-        return len(self.values)
+        return len(self.__values)
