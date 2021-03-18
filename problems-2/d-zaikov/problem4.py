7c5d565ea1bebac87b081c6062dc218617c9f0e7
dzaikov <diman2000gzz@gmail.com>
2021-03-18 16:13:20 +0700

    переделал исключение и заменил слайс на read(2)

--- problems-2/d-zaikov/problem4.py
+++ problems-2/d-zaikov/problem4.py
@@ -14,7 +14,8 @@ def find_seq(seq, path):
         return cnt, pos_list
     
     with open(path, 'r') as file:
-        pi = file.read()[2:].replace('\n', '')
+        file.read(2)
+        pi = file.read().replace('\n', '')
     
     
    
@@ -46,26 +47,11 @@ if __name__ == '__main__':
         exit(1)
     try:
         cnt, posl = find_seq(seq, path)
-    except FileNotFoundError as e:
-        print('Error: the entered path to the file with pi number not found.\n', file=sys.stderr)
-        print('--------More info--------', file=sys.stderr)
-        print(e, file=sys.stderr)
-        exit(1)
-    except IsADirectoryError as e:
-        print('Error: the entered path to the file with pi number is not a file.\n', file=sys.stderr)
-        print('--------More info--------', file=sys.stderr)
-        print(e, file=sys.stderr)
-        exit(1)
-    except PermissionError as e:
-        print('Error: you have not permission to read the file with pi number by the entered path.\n', file=sys.stderr)
-        print('--------More info--------', file=sys.stderr)
-        print(e, file=sys.stderr)
-        exit(1)
     except Exception as e:
-        print('Error: some error occurred while trying to read the file with pi number.\n', file=sys.stderr)
-        print('--------More info--------', file=sys.stderr)
+        print('Error while trying to call "open()"', file=sys.stderr)
         print(e, file=sys.stderr)
         exit(1)
+    
     print(f'Found {cnt} results.')
     print(f'Positions: ', end='')
     for p in posl:
