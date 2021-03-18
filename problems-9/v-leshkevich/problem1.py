c45a5f1bf3310f02ceccdf1dcbbc77bb1caf353f
Jlemix <unuashechka@gmail.com>
2021-03-18 10:37:04 +0700

    Add try/catch/finally statements

--- problems-9/v-leshkevich/problem1.py
+++ problems-9/v-leshkevich/problem1.py
@@ -10,6 +10,10 @@ class Timer:
     @contextmanager
     def timer(self):
         start_time = time()
-        yield
-        self.exec_time = int(round(time() - start_time)) * 1000
-        print("'with' block execution time: {time} ms".format(time=self.exec_time))
+        try:
+            yield
+        except Exception as e:
+            print('Caught exception: ' + str(e))
+        finally:
+            self.exec_time = int(round(time() - start_time)) * 1000
+            print("'with' block execution time: {time} ms".format(time=self.exec_time))
