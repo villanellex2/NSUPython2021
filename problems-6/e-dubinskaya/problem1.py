35e4dbd3a1f3ba4edf94fe842f2d64a9a4f708e3
katerina <bromkate@mail.ru>
2021-04-29 13:10:02 +0700

    fix spaces

--- problems-6/e-dubinskaya/problem1.py
+++ problems-6/e-dubinskaya/problem1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import re
 import sys
@@ -6,7 +7,7 @@ import random
 
 def sort_word(elem: str) -> str:
     if len(elem) == 1:
-        red = re.match(r"[^\w\s]", elem)
+        red = re.match(r"[,.;:\"\'!)]", elem)
         if red is not None and red.pos >= 0:
             return elem
         else:
@@ -27,32 +28,33 @@ def random_word(elem: str) -> str:
         return " " + elem[0] + "".join(random.sample(arr[1:-1], len(arr) - 2)) + elem[len(elem) - 1]
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--sort')
-parser.add_argument('-r', '--random')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sort')
+    parser.add_argument('-r', '--random')
 
-args = parser.parse_args()
-fun = sort_word
-file = ""
-
-if args.sort:
+    args = parser.parse_args()
     fun = sort_word
-    file = args.sort
-elif args.random:
-    file = args.random
-    fun = random_word
-else:
-    print("No flag set, you should use -s or -r", file=sys.stderr)
-    exit()
+    file = ""
 
-try:
-    f = open(file, 'r')
-    line = f.readline()
-    while line:
-        words = re.findall(r"\w+|[^\w\s]", line, re.UNICODE)
-        print(''.join(map(fun, words)), end="")
+    if args.sort:
+        fun = sort_word
+        file = args.sort
+    elif args.random:
+        file = args.random
+        fun = random_word
+    else:
+        print("No flag set, you should use -s or -r", file=sys.stderr)
+        exit()
+
+    try:
+        f = open(file, 'r')
         line = f.readline()
-        print()
+        while line:
+            words = re.findall(r"\w+|[^\w\s]", line)
+            print(''.join(map(fun, words)), end="")
+            line = f.readline()
+            print()
 
-except OSError as e:
-    print(e, file=sys.stderr)
+    except OSError as e:
+        print(e, file=sys.stderr)
